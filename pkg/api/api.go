f2ed093c6b0daa43784774e37a038bfe2312c16e
hendisantika <hendisantika@yahoo.co.id>
2024-08-10 18:05:26 +0700

    Add CommentsResponse 🫸🌀✏️📗🐧🐳

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -42,3 +42,9 @@ type CommentResponse struct {
 	Error   string          `json:"error"`
 	Comment *models.Comment `json:"comment"`
 }
+
+type CommentsResponse struct {
+	Success  bool              `json:"success"`
+	Error    string            `json:"error"`
+	Comments []*models.Comment `json:"comments"`
+}
