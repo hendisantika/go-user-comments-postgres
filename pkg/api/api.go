e39ac2570c8823f9e355d4398e7d87f87f2526d7
hendisantika <hendisantika@yahoo.co.id>
2024-08-10 18:04:22 +0700

    Add StartAPI 🫸🌀✏️📗🐧🐳

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -1,1 +1,31 @@
 package api
+
+import (
+	"github.com/go-chi/chi/v5/middleware"
+	"net/http"
+)
+
+// start api with the pgdb and return a chi router
+func StartAPI(pgdb *pg.DB) *chi.Mux {
+	//get the router
+	r := chi.NewRouter()
+	//add middleware
+	//in this case we will store our DB to use it later
+	r.Use(middleware.Logger, middleware.WithValue("DB", pgdb))
+
+	//routes for our service
+	r.Route("/comments", func(r chi.Router) {
+		r.Post("/", createComment)
+		r.Get("/", getComments)
+		r.Get("/{commentID}", getCommentByID)
+		r.Put("/{commentID}", updateCommentByID)
+		r.Delete("/{commentID}", deleteCommentByID)
+	})
+
+	//test route to make sure everything works
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("up and running"))
+	})
+
+	return r
+}
