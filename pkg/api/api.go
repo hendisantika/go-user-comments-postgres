34e27d6920f5cac153d8cded4d413dc151bafac2
hendisantika <hendisantika@yahoo.co.id>
2024-08-10 18:05:53 +0700

    Add handleDBFromContextErr 🫸🌀✏️📗🐧🐳

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -66,3 +66,18 @@ func handleErr(w http.ResponseWriter, err error) {
 	//return a bad request and exist the function
 	w.WriteHeader(http.StatusBadRequest)
 }
+
+func handleDBFromContextErr(w http.ResponseWriter) {
+	res := &CommentResponse{
+		Success: false,
+		Error:   "could not get the DB from context",
+		Comment: nil,
+	}
+	err := json.NewEncoder(w).Encode(res)
+	//if there's an error with encoding handle it
+	if err != nil {
+		log.Printf("error sending response %v\n", err)
+	}
+	//return a bad request and exist the function
+	w.WriteHeader(http.StatusBadRequest)
+}
